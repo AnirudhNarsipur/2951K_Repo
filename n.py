9d4fa324c271423970f2c41733262ecfd031f2eb
Anirudh Narsipur <anirudh.narsipur@gmail.com>
2023-04-04 11:22:56 -0400

    r

--- n.py
+++ n.py
@@ -92,9 +92,9 @@ def map_keyboard():
         'b': (grip_right.calibrate, [], "right: gripper calibrate"),
      }
     left = baxter_interface.Limb('left')
-    right = baxter_interface.Limb('left')
+    right = baxter_interface.Limb('right')
     print("left join_states",left.joint_angles())
-    
+
     print("right joint states ",right.joint_angles(),"\n")
     rospy.signal_shutdown("Example finished.")
 
