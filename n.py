49e1159ada25b11e3a4f6df0cb6979d0e7521dd4
Anirudh Narsipur <anirudh.narsipur@gmail.com>
2023-04-04 11:44:52 -0400

    ds

--- n.py
+++ n.py
@@ -99,7 +99,8 @@ def map_keyboard():
     left = baxter_interface.Limb('left')
     right = baxter_interface.Limb('right')
     print("left join_states",left.joint_angles())
-    print("left cartesian pose" , left._cartesian_pose)
+    print("left enpoint pose" , left.endpoint_pose())
+    
     print("right joint states ",right.joint_angles(),"\n")
     set_arm_pos(left,default_l_pos)
     set_arm_pos(right,default_r_pos)
