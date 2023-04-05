9e0e5feb53d0a33694eb833b4406df22c3c7b5b9
Julia McCauley <juliamccauley@julias-mbp-50.devices.brown.edu>
2023-04-05 10:11:40 -0400

    trying different movement command

--- n.py
+++ n.py
@@ -59,13 +59,13 @@ def map_keyboard():
     
     
     def set_arm_pos(limb,pos_dict):
-        limb.set_joint_positions(pos_dict)
-        diff =vector_diff(limb.joint_angles(),pos_dict)
-        while diff > 0.01:
-            limb.set_joint_positions(pos_dict)
-            print("vector diff is ",diff)
-            print(limb.joint_angles())
-            diff =vector_diff(limb.joint_angles(),pos_dict)
+        limb.move_to_joint_positions(pos_dict)
+        # diff =vector_diff(limb.joint_angles(),pos_dict)
+        # while diff > 0.01:
+        #     limb.set_joint_positions(pos_dict)
+        #     print("vector diff is ",diff)
+        #     print(limb.joint_angles)
+        #     diff =vector_diff(limb.joint_angles(),pos_dict)
             # y=raw_input("break?")
             # if y=="y":
                 # break
